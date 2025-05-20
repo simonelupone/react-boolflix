3ec43ee17b236aae0ea685b21ff1a3d98c41b0ab
simonelupone <simone.lupone@gmail.com>
2025-05-20 15:01:14 +0200

    feat: add form with style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
-  )
-}
+    <div className="w-full flex justify-center">
+      <div className="w-7xl max-w-sm p-4">
+        <form className="w-full max-w-sm">
+          <div className="flex items-center border-b border-red-500 py-2">
+            <input
+              className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+              type="text"
+              placeholder="Search..."
+            />
+            <button
+              className="flex-shrink-0 bg-red-500 hover:bg-red-700 border-red-500 hover:border-red-700 text-sm border-4 text-white py-1 px-2 rounded"
+              type="submit"
+            >
+              Search
+            </button>
+            <button
+              className="flex-shrink-0 border-transparent border-4 text-red-500 hover:text-red-800 text-sm py-1 px-2 rounded"
+              type="button"
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
